fa6eef2c0effa5cda1f1abe64e13175d33a7ff86
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-04-12 21:51:04 +0300

    Bring back the image tag for all the cars

--- src/modules/displaycars.js
+++ src/modules/displaycars.js
@@ -5,6 +5,7 @@ const carsContainer = document.getElementById('vehicles-container');
 const loadVehicles = async () => {
   const data = await getCars();
   carsContainer.innerHTML = data.slice(0, 6).map((car) => `      <div class="v-item">
+  <img src="${car.img_url}" alt="${car.model}">
   <div>
     <h2>${car.make}</h2>
     <p>3 Likes</p>
