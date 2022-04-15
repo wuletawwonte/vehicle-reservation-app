b9c81cf906c69c1d43715960af983243f2986eba
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-04-15 15:46:40 +0300

    Fix eslint error

--- src/__test__/itemscounter.test.js
+++ src/__test__/itemscounter.test.js
@@ -5,11 +5,11 @@ describe('total vehicles: ', () => {
   let cars;
   beforeEach(() => {
     cars = new Cars();
-  })
+  });
   test('equals item count', async () => {
     const total = 8;
-    cars.count().then(data => {
+    cars.count().then((data) => {
       expect(data).toEqual(total);
-    }).catch(err => console.log(err));
-  })
-})
+    }).catch((err) => console.error(err));
+  });
+});
