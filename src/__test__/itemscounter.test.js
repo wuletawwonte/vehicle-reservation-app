83a57ba93d964d9e0fb000f33ea4d94e5d0358b3
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-04-15 12:32:40 +0300

    Add babel in the package.json file and install it

---
+++ src/__test__/itemscounter.test.js
@@ -0,0 +1,13 @@
+import Cars from '../modules/cars.js';
+
+describe('total vehicles: ', () => {
+  let cars;
+  beforeEach(() => {
+    cars = new Cars();
+  })
+  test('equals item count', async () => {
+    const total = 6;
+    const totalCars = await cars.count();
+    expect(totalCars).toEqual(total);
+  })
+})
